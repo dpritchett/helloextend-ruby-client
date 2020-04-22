1af60b3741290930847f69ee99a31d7e31ea0146
Daniel J. Pritchett <dpritchett@gmail.com>
2020-04-22 13:53:52 -0500

    Adds readme and demo screencast

--- Makefile
+++ Makefile
@@ -1,2 +1,16 @@
-build:
+all: clean build-ruby-client install-gem ruby-product-demo
+
+build-ruby-client:
 	env OUTPUT_LANGUAGE=ruby ./shell/generate-bindings.sh
+
+install-gem:
+	cd ./clients/ruby && \
+	gem build openapi_client.gemspec && \
+	gem install ./openapi_client-1.0.0.gem
+
+clean:
+	gem uninstall openapi_client
+	rm -rf ./clients/ruby
+
+ruby-product-demo:
+	ruby ./test_client.rb
--- clients/ruby/spec/configuration_spec.rb
+++ clients/ruby/spec/configuration_spec.rb
@@ -18,7 +18,7 @@ describe OpenapiClient::Configuration do
   before(:each) do
     # uncomment below to setup host and base_path
     # require 'URI'
-    # uri = URI.parse("https://api.helloextend.com")
+    # uri = URI.parse("https://api-demo.helloextend.com")
     # OpenapiClient.configure do |c|
     #   c.host = uri.host
     #   c.base_path = uri.path
@@ -28,14 +28,14 @@ describe OpenapiClient::Configuration do
   describe '#base_url' do
     it 'should have the default value' do
       # uncomment below to test default value of the base path
-      # expect(config.base_url).to eq("https://api.helloextend.com")
+      # expect(config.base_url).to eq("https://api-demo.helloextend.com")
     end
 
     it 'should remove trailing slashes' do
       [nil, '', '/', '//'].each do |base_path|
         config.base_path = base_path
         # uncomment below to test trailing slashes
-        # expect(config.base_url).to eq("https://api.helloextend.com")
+        # expect(config.base_url).to eq("https://api-demo.helloextend.com")
       end
     end
   end
